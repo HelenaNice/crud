bb2dbfb8c97e218ad3fb66ab1eaac1717a7b1a6e
HelenaNice <nechalena@gmail.com>
2023-07-29 13:13:26 +0300

    Add_purchase card

--- src/route/index.js
+++ src/route/index.js
@@ -4,123 +4,176 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
-class User {
+class Product {
   static #list = []
+  static #count = 0
+  //для унікальності ідентификаторів
 
-  constructor(email, login, password) {
-    this.email = email
-    this.login = login
-    this.password = password
-    this.id = new Date().getTime()
+  constructor(
+    img,
+    title,
+    description,
+    category,
+    price,
+    amount = 0, // кількість товару- 0 за умовч
+  ) {
+    this.id = ++Product.#count // Генеруємо унікальний id для товару
+    // кожен раз доданний товар збільшує count і додаеться в id, а при видвлені товара
+    // наш count не змінюється, тож настуній товар матиме последовниу номер id.
+    this.img = img
+    this.title = title
+    this.description = description
+    this.category = category
+    this.price = price
+    this.amount = amount
+  }
+  // метод, що додає товар
+  static add = (...data) => {
+    const newProduct = new Product(...data)
+
+    this.#list.push(newProduct)
   }
 
-  verifyPassword = (password) => this.password === password
-
-  static add = (user) => {
-    this.#list.push(user)
-  }
-
-  static getList = () => this.#list
+  static getList = () =>
+    // метод, що формує список
+    {
+      return this.#list
+    }
   static getById = (id) => {
-    return this.#list.find((user) => user.id === id)
+    // знаходить продукт по нашому іденту
+    return this.#list.find((product) => product.id === id)
   }
-
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (user) => user.id === id,
+  static getRandomList = (id) => {
+    // Фільтруємо товари, щоб вилучити той, з яким порівнюємо id
+    const filteredList = this.#list.filter(
+      (product) => product.id !== id,
     )
 
-    if (index !== -1) {
-      this.#list.splice(index, 1)
-      return true
-    } else {
-      return false
-    }
-  }
+    // Відсортуємо за допомогою .Math.random() та перемішаємо масив
+    const shuffledList = filteredList.sort(
+      () => Math.random() - 0.5,
+    )
 
-  static updateById = (id, data) => {
-    const user = this.getById(id)
-
-    if (user) {
-      this.update(user, data)
-
-      return true
-    } else {
-      return false
-    }
-  }
-  static update = (user, { email }) => {
-    if (email) {
-      user.email = email
-    }
+    // Повертаємо перші 3 елементи з перемішаного масиву
+    return shuffledList.slice(0, 3)
   }
 }
+// додали тестові товари
+Product.add(
+  'https://picsum.photos/200/300',
+  `Комп'ютер Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600`,
+  'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС',
+  [
+    { id: 1, text: 'Готовий до відправки' },
+    { id: 2, text: 'Топ продажів' },
+  ],
+  27000,
+  10,
+)
+
+Product.add(
+  'https://picsum.photos/200/300',
+  `Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel`,
+  'Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux',
+  [
+    // { id: 1, text: 'Готовий до відправки' },
+    { id: 2, text: 'Топ продажів' },
+  ],
+  20000,
+  10,
+)
+Product.add(
+  'https://picsum.photos/200/300',
+  `Комп'ютер ARTLINE Gaming by ASUS TUF v119 (TUFv119)`,
+  'Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / / без ОС',
+  [
+    { id: 1, text: 'Готовий до відправки' },
+    // { id: 2, text: 'Топ продажів' },
+  ],
+  40000,
+  10,
+)
+// ==
 // ================================================================
 // router.get Створює нам один ендпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  const list = User.getList()
-
-  res.render('index', {
-    style: 'index',
+  res.render('purchase-index', {
+    style: 'purchase-index',
     data: {
-      users: {
-        list: User.getList(),
-        isEmpty: list.length === 0,
-      },
+      list: Product.getList(),
     },
   })
 })
 
-// ================================================================
-router.post('/user-create', function (req, res) {
-  const { email, login, password } = req.body
-
-  const user = new User(email, login, password)
-  User.add(user)
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Користувач створений',
+//=====GET=============== новий ендпонт
+router.get('/purchase-product', function (req, res) {
+  const id = Number(req.query.id) //отримуемо число ідентифікатор від req.query.id,!інакше отримуємо ряок
+  // вказує назву папки з container
+  res.render('purchase-product', {
+    // вказує назву папки-контейра, де знаходяться наші стилі
+    style: 'purchase-product',
+    // повертаємо спис
+    data: {
+      list: Product.getRandomList(id),
+      product: Product.getById(id),
+    },
   })
 })
-// ================================================================
-router.get('/user-delete', function (req, res) {
-  const { id } = req.query
+//====POST================== новий ендпонт
+router.post('/purchase-create', function (req, res) {
+  const id = Number(req.query.id) //отримуемо число ідентифікатор через запит (query parameter) ,!інакше отримуємо ряок
+  const amount = Number(req.body.amount) //отримуемо число amount через тело запроса (request body).
+  //щоб відслідковувати потік даних
 
-  User.deleteById(Number(id))
-
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Користувача видалено',
-  })
-})
-
-// ================================================================
-router.post('/user-update', function (req, res) {
-  const { email, password, id } = req.body
-
-  let result = false
-
-  const user = User.getById(Number(id))
-
-  result = false
-
-  if (user.verifyPassword(password)) {
-    User.update(user, { email })
-    result = true
+  console.log(id, amount)
+  if (amount < 1) {
+    return res.render('alert', {
+      style: 'alert',
+      // повертаємо алерт та посилання назад до обранного товару
+      data: {
+        message: 'Помилка',
+        info: 'Некоректна кількість товару',
+        link: '/purchase-product?id=${id}',
+      },
+    })
   }
 
-  // const result = User.updateById(Number(id), { email })
+  const product = Product.getById(id)
 
-  res.render('success-info', {
-    style: 'success-info',
-    info: result
-      ? 'Електронна пошта оновлена'
-      : 'Сталася помилка при оновленні даних',
+  if (product.amount < 1) {
+    return res.render('alert', {
+      style: 'alert',
+      // повертаємо алерт та посилання назад до обранного товару
+      data: {
+        message: 'Помилка',
+        info: 'Такої кількості товару немає в наявности',
+        link: '/purchase-product?id=${id}',
+      },
+    })
+  }
+
+  console.log(product, amount)
+
+  // вказує назву папки з container
+  res.render('purchase-product', {
+    // вказує назву папки-контейра, де знаходяться наші стилі
+    style: 'purchase-product',
+    // повертаємо спис
+    data: {
+      list: Product.getRandomList(id),
+      product: Product.getById(id),
+    },
   })
 })
+
+//  ================================================================
+
+// ================================================================
+
+// const result = User.updateById(Number(id), { email })
+
 // ================================================================
 
 // Підключаємо роутер до бек-енду
