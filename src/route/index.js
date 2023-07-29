304145d316f2d5058c8a46e04d55bc45f80b3e00
HelenaNice <nechalena@gmail.com>
2023-07-30 00:05:15 +0300

    finish_1part

--- src/route/index.js
+++ src/route/index.js
@@ -19,7 +19,7 @@ class Product {
   ) {
     this.id = ++Product.#count // Генеруємо унікальний id для товару
     // кожен раз доданний товар збільшує count і додаеться в id, а при видвлені товара
-    // наш count не змінюється, тож настуній товар матиме последовниу номер id.
+    // наш count не змінюється, тож настуній товар матиме последовний номер id.
     this.img = img
     this.title = title
     this.description = description
@@ -93,7 +93,125 @@ Product.add(
   40000,
   10,
 )
-// ==
+class Purchase {
+  static DELIVERY_PRICE = 150 // це статична ціна замовлення
+  static #BONUS_FACTOR = 0.1
+
+  static #count = 0
+  static #list = []
+
+  static #bonusAccount = new Map() // словник. в форматі ключ-знач буде йти пошта та бонусбаланс
+
+  static getBonusBalance = (email) => {
+    return Purchase.#bonusAccount.get(email) || 0 // стат метод.шукае бонус по пошті та повертае значення, або нуль
+  }
+
+  // щоб використовувати значення статичного метода #BONUS_FACTOR,
+  static calcBonusAmount = (value) => {
+    return value * Purchase.#BONUS_FACTOR
+  }
+
+  static updateBonusBalance = (
+    email,
+    price,
+    bonusUse = 0, // скільки бонусів бажає списати користувач, з/з=0
+  ) => {
+    const amount = this.calcBonusAmount(price)
+
+    const currentBalance = Purchase.getBonusBalance(email)
+
+    const updatedBalance =
+      currentBalance + amount - bonusUse
+
+    Purchase.#bonusAccount.set(email, updatedBalance)
+
+    console.log(email, updatedBalance)
+
+    return amount
+  }
+
+  constructor(data, product) {
+    this.id = ++Purchase.#count
+
+    this.firstname = data.firstname
+    this.lastname = data.lastname
+
+    this.phone = data.phone
+    this.email = data.email
+
+    this.comment = data.comment || null
+
+    this.bonus = data.bonus || 0
+
+    this.promocode = data.promocode || null
+
+    this.totalPrice = data.totalPrice
+    this.productPrice = data.productPrice
+    this.deliveryPrice = data.deliveryPrice
+    this.amount = data.amount
+
+    this.product = product
+  }
+  // статичні методи
+  static add = (...arg) => {
+    const newPurchase = new Purchase(...arg)
+
+    this.#list.push(newPurchase)
+    return newPurchase
+  }
+  static getList = () => {
+    return Purchase.#list.reverse() //  отримаем список замовлень (спочатку нові)
+    // !!!! - самостійно через деструктурізацію .map(({...}) => {...}) дістаєм дані та повертаємоу  у обїєкт,  щоб отримати потрібні
+    //  тільки потрібні поля: id, бонуси
+  }
+
+  static getById = (id) => {
+    return Purchase.#list.find((item) => item.id === id) // знаходить\повертає товар по айди
+  }
+  static updateById = (id, data) => {
+    const purchase = Purchase.getById(id)
+    // передаємо потрібні поля, якщо вони присутні в data
+    if (purchase) {
+      if (data.firstname)
+        purchase.firstname = data.firstname
+      if (data.lastname) purchase.lastname = data.lastname
+      if (data.phone) purchase.phone = data.phone
+      if (data.email) purchase.email = data.email
+
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+// логика списання та нарахування бонусів
+class Promocode {
+  static #list = []
+
+  constructor(name, factor) {
+    this.name = name
+    this.factor = factor
+  }
+
+  static add = (name, factor) => {
+    const newPromoCode = new Promocode(name, factor)
+    Promocode.#list.push(newPromoCode)
+    return newPromoCode
+  }
+  // шукає промокод по назві
+  static getByName = (name) => {
+    return this.#list.find((promo) => promo.name === name)
+  }
+
+  static calc = (promo, price) => {
+    return price * promo.factor
+  }
+}
+Promocode.add('SUMMER2023', 0.9)
+Promocode.add('DISCOUNT50', 0.5)
+Promocode.add('SALE25', 0.75)
+
 // ================================================================
 // router.get Створює нам один ендпоїнт
 
@@ -127,7 +245,6 @@ router.post('/purchase-create', function (req, res) {
   const amount = Number(req.body.amount) //отримуемо число amount через тело запроса (request body).
   //щоб відслідковувати потік даних
 
-  console.log(id, amount)
   if (amount < 1) {
     return res.render('alert', {
       style: 'alert',
@@ -135,7 +252,7 @@ router.post('/purchase-create', function (req, res) {
       data: {
         message: 'Помилка',
         info: 'Некоректна кількість товару',
-        link: '/purchase-product?id=${id}',
+        link: `/purchase-product?id=${id}`,
       },
     })
   }
@@ -148,31 +265,181 @@ router.post('/purchase-create', function (req, res) {
       // повертаємо алерт та посилання назад до обранного товару
       data: {
         message: 'Помилка',
-        info: 'Такої кількості товару немає в наявности',
-        link: '/purchase-product?id=${id}',
+        info: 'Такої кількості товару немає в наявності',
+        link: `/purchase-product?id=${id}`,
       },
     })
   }
 
   console.log(product, amount)
 
-  // вказує назву папки з container
-  res.render('purchase-product', {
-    // вказує назву папки-контейра, де знаходяться наші стилі
-    style: 'purchase-product',
-    // повертаємо спис
+  const productPrice = product.price * amount
+  const totalPrice = productPrice + Purchase.DELIVERY_PRICE
+  const bonus = Purchase.calcBonusAmount(totalPrice)
+
+  // ************вказує назву папки з container
+  res.render('purchase-create', {
+    style: 'purchase-create',
+
     data: {
-      list: Product.getRandomList(id),
-      product: Product.getById(id),
+      id: product.id,
+
+      cart: [
+        {
+          text: `${product.title} (${amount} шт)`,
+          price: productPrice,
+        },
+        {
+          text: `Доставка`,
+          price: Purchase.DELIVERY_PRICE,
+        },
+      ],
+      totalPrice,
+      productPrice,
+      deliveryPrice: Purchase.DELIVERY_PRICE,
+      amount,
+      bonus,
     },
   })
 })
 
-//  ================================================================
+// ======POST========
+router.post('/purchase-submit', function (req, res) {
+  const id = Number(req.query.id)
 
+  let {
+    totalPrice,
+    productPrice,
+    deliveryPrice,
+    amount,
+
+    firstname,
+    lastname,
+    email,
+    phone,
+    comment,
+
+    promocode,
+    bonus,
+  } = req.body
+
+  const product = Product.getById(id)
+
+  if (!product) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Товар не знайдено',
+        link: `/purchase-list`,
+      },
+    })
+  }
+  // перевірка кількості товара  - наявній
+  if (product.amount < amount) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Товару немає в потрібній кількості',
+        link: `/purchase-list`,
+      },
+    })
+  }
+  totalPrice = Number(totalPrice)
+  productPrice = Number(productPrice)
+  deliveryPrice = Number(deliveryPrice)
+  amount = Number(amount)
+  bonus = Number(bonus)
+  // перевірка коректності введених значень
+  if (
+    isNaN(totalPrice) ||
+    isNaN(productPrice) ||
+    isNaN(deliveryPrice) ||
+    isNaN(amount) ||
+    isNaN(bonus)
+  ) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Некоректні дані',
+        link: `/purchase-list`,
+      },
+    })
+  }
+  // чи заповнені поля?
+  if (!firstname || !lastname || !email || !phone) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: `Заповніть обов'язкові поля`,
+        info: 'Некоректні дані',
+        link: `/purchase-list`,
+      },
+    })
+  }
+  // чи є бонус, та він більше 0?
+  if (bonus || bonus > 0) {
+    const bonusAmount = Purchase.getBonusBalance(email) // отримали кіл-ть бонусів на поточному балансі
+
+    console.log(bonusAmount)
+    // якщо число  бонусів перевищує кіл-сть наявних бонусів на поточному балансі, обмежуем наявними
+    if (bonus > bonusAmount) {
+      bonus = bonusAmount
+    }
+
+    Purchase.updateBonusBalance(email, totalPrice, bonus) // 1)обновили баланс с нарахованими бонусами
+
+    totalPrice -= bonus // зменьшуємо ціну на кіл-сть бонусів
+    // 2)якщо не має введених бонусів, то вважаємо, що =0
+  } else {
+    Purchase.updateBonusBalance(email, totalPrice, 0)
+  }
+
+  // чи є промокод. Він рахується після бонусу
+  if (promocode) {
+    promocode = Promocode.getByName(promocode) // шукаемо по назві промокод, и кладемо в змінну promocode. Якщо є →
+
+    if (promocode) {
+      totalPrice = Promocode.calc(promocode, totalPrice) //  → переписуєм totalPrice на калькуляцию totalPrice*promocode
+    }
+  }
+
+  if (totalPrice < 0) totalPrice = 0 // навіть за відємного числа прайса, вважаємо, що це 0.
+
+  // після додавання всіх оновлень, знижок отримуємо нові значення const purchase
+  const purchase = Purchase.add(
+    {
+      totalPrice,
+      productPrice,
+      deliveryPrice,
+      amount,
+      bonus,
+
+      firstname,
+      lastname,
+      email,
+      phone,
+
+      promocode,
+      comment,
+    },
+    product,
+  )
+
+  console.log(purchase)
+
+  res.render('alert', {
+    style: 'alert',
+    data: {
+      message: 'Успішно',
+      info: 'Замовлення створено',
+      link: `/purchase-list`,
+    },
+  })
+})
 // ================================================================
-
-// const result = User.updateById(Number(id), { email })
 
 // ================================================================
 
